6f659ba0df1b87869a9fc7b3d34c26f2d7024dbd
Inge Anundskås <42357272+ingean@users.noreply.github.com>
2021-03-04 11:57:02 +0100

    Vehicle list updates

--- modules/Vehicles.js
+++ modules/Vehicles.js
@@ -6,19 +6,10 @@ function(QueryTask, Query) {
 
   const url = 'https://us-iot.arcgis.com/d8avj4l9dv7mdfsa/d8avj4l9dv7mdfsa/maps/arcgis/rest/services/e16_features_Update/FeatureServer/0';
  
-  function getFeatures() {
-    let queryTask = new QueryTask({
-      url: url
-    });
-    let query = new Query();
-    query.returnGeometry = true;
-    query.outFields = ["*"];
-    query.where = "1=1"; 
-  
-    queryTask.execute(query)
-    .then(results => {
-      updateVehicleList(results.features);
-    });
+  function createDiv(className) {
+    let div = document.createElement('div');
+    div.className = className;
+    return div
   }
 
   function updateVehicles() {
@@ -43,63 +34,69 @@ function(QueryTask, Query) {
   }
 
   function updateVehicleList(features) {
-    
     let count = features.length
     let sumload = 0;
     let maxspeed = 0;
     
     for (feature of features) {
-
       let a = feature.attributes;
       let item  = document.getElementById(a.globalid);
 
       (item) ? updateListItem(a) : addListItem(a);
       sumload += a.Quantity;
       if (a.Speed > maxspeed) maxspeed = a.Speed;
-        
     }
+    updateVehicleIndicators(count, sumload, maxspeed);
+  }
 
+  function updateVehicleIndicators(count, sumload, maxspeed) {
     document.getElementById('indicator-vehicles').innerHTML = count;
     document.getElementById('indicator-sumload').innerHTML = sumload.toLocaleString() + ' m<sup>3</sup>';
-    document.getElementById('indicator-maxspeed').innerHTML = Math.round(maxspeed) + ' km/t';
+    let sText = document.getElementById('indicator-maxspeed');
+    sText.innerHTML = Math.round(maxspeed) + ' km/t';
+    let sIcon = sText.nextSibling;
 
+    if (maxspeed >= 8) {
+      sText.className = 'i-text i-text-alert';
+      sIcon.className = 'icon-ui-chart icon-ui-red';
+    } else {
+      sText.className = 'i-text';
+      sIcon.className = 'icon-ui-chart';
+    } 
   }
+
+
 
   function addListItem(feature) {
     let list = document.getElementById('machine-list');
-    let item = document.createElement('div');
-    let icon = document.createElement('div');
-    let textcontainer = document.createElement('div');
-    let header = document.createElement('div');
-    let body = document.createElement('div');
-    let footer = document.createElement('div');
-    
-    item.className = 'db-list-item';
-    icon.className = 'icon-ui-grant';
-    if (feature.Speed > 6) icon.className = 'icon-ui-deny icon-ui-red'
-    textcontainer.className = 'db-list-text';
-    header.className = 'db-list-text db-list-text-header';
-    body.className = 'db-list-text db-list-text-body';
-    footer.className = 'db-list-text db-list-text-footer';
 
+    let item = createDiv('db-list-item');
+    let icon = createDiv('icon-ui-grant');
+    let text = createDiv('db-list-text');
+    let header = createDiv('db-list-text db-list-text-header');
+    let body = createDiv('db-list-text db-list-text-body');
+    let footer = createDiv('db-list-text db-list-text-footer');
+    if (feature.Speed >= 8) icon.className = 'icon-ui-deny icon-ui-red'
+    
+    
     item.id = feature.globalid
     updateItem(item, header, body, footer, feature);
     
-    textcontainer.appendChild(header);
-    textcontainer.appendChild(body);
-    textcontainer.appendChild(footer);
+    text.appendChild(header);
+    text.appendChild(body);
+    text.appendChild(footer);
 
     item.appendChild(icon);
-    item.appendChild(textcontainer);
+    item.appendChild(text);
     list.appendChild(item);
   }
 
   function updateListItem(feature) {
     let item = document.getElementById(feature.globalid);
-    let textcontainer = item.childNodes[1];
-    let header = textcontainer.childNodes[0];
-    let body = textcontainer.childNodes[1];
-    let footer = textcontainer.childNodes[2];
+    let text = item.childNodes[1];
+    let header = text.childNodes[0];
+    let body = text.childNodes[1];
+    let footer = text.childNodes[2];
      updateItem(item, header, body, footer, feature);
   }
 
