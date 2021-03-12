2cb1ea61a393cfd936844d805a7456a40943eb9d
Inge Anundskås <42357272+ingean@users.noreply.github.com>
2021-03-12 09:17:17 +0100

    Oppdaterte BIM-modeller

--- modules/Config.js
+++ modules/Config.js
@@ -13,11 +13,11 @@ define([], function() {
     },
     segments: {
       1: {name: 'Åsbygda'},
-      3: {slide: 2, name: 'Kleggerud', volume: true, BIM: 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Z2VvdGVrXzIwMjEvZiVFMiU4MCU5MGJydV9LMDMtS2xlZ2dlcnVkLW92ZXJnYW5nc2JydS5ydnQ'},
-      5: {slide: 3, name: 'Moselva', volume: true, BIM: 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Z2VvdGVrXzIwMjEvZi1icnVfSzA1LU1vc2VsdmEtYnJ1MDEucnZ0'},
-      7: {slide: 4, name: 'Svenådalen', volume: true,},
-      9: {slide: 5, name: 'Søtbakkdalen', volume: true, BIM: 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Z2VvdGVrXzIwMjEvZi1icnVfSzA5LVMlQzMlQjh0YmFra2RhbGVuLW92ZXJnYW5nc2JydS5ydnQ'},
-      10: {slide: 6, volume: true, BIM: 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Z2VvdGVrXzIwMjEvZi1icnVfSzEwLUJla2tlc3R1YS1icnUucnZ0'},
+      3: {slide: 2, name: 'Kleggerud', volume: true, BIM: 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Z2VvdGVrXzIwMjEvUDAzLUtsZWdnZXJ1ZC1vdmVyZ2FuZ3NicnUucnZ0'},
+      5: {slide: 3, name: 'Moselva', volume: true, BIM: 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Z2VvdGVrXzIwMjEvUDA1LU1vc2VsdmEtYnJ1LnJ2dA'},
+      7: {slide: 4, name: 'Svenådalen', volume: true, BIM: 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Z2VvdGVrXzIwMjEvUDAzLUtsZWdnZXJ1ZC1vdmVyZ2FuZ3NicnUucnZ0'},
+      9: {slide: 5, name: 'Søtbakkdalen', volume: true, BIM: 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Z2VvdGVrXzIwMjEvUDA5LVMlQzMlQjh0YmFra2RhbGVuLW92ZXJnYW5nc2JydS5ydnQ'},
+      10: {slide: 6, volume: true, BIM: 'urn:dXJuOmFkc2sub2JqZWN0czpvcy5vYmplY3Q6Z2VvdGVrXzIwMjEvUDEwLUJla2tlc3R1YS1icnUucnZ0'},
       21: {name: 'Olum'},
       count: 21
     }
